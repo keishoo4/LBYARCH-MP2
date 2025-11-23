43d019b7c075b31ac1876ae2f9350cd07bbc1732
Deo Diamante <diamantedeozamir@gmail.com>
2025-11-23 20:00:56 +0800

    Adds x20 run loop for c and asm

--- main.c
+++ main.c
@@ -41,32 +41,58 @@ int main (){
     }
 	
 	// Start timing C implementation
-	printf("Starting dot product calculation (C)...\n");
-	clock_t start_time_c = clock();
+	float total_time_c = 0.0f;
+	printf("Starting dot product calculation (C) x20...\n");
+	for (int t = 0; t < 20; t++) {
+		clock_t start = clock();
+		result_c = dot_product_c(vec1, vec2, n);
+		clock_t end = clock();
+		total_time_c += ((float)(end - start)) / CLOCKS_PER_SEC;
+	}
+	float avg_time_c = total_time_c / 20.0f;
+
+	
+	// printf("Starting dot product calculation (C)...\n");
+	// clock_t start_time_c = clock();
 	
 	// Perform single dot product calculation (C)
-	result_c = dot_product_c(vec1, vec2, n);
+	// result_c = dot_product_c(vec1, vec2, n);
 	
-	clock_t end_time_c = clock();
-	float execution_time_c = ((float)(end_time_c - start_time_c)) / CLOCKS_PER_SEC;
+	// clock_t end_time_c = clock();
+	// float execution_time_c = ((float)(end_time_c - start_time_c)) / CLOCKS_PER_SEC;
 
+
+	// Start timing C implementation
+	float total_time_asm = 0.0f;
 	printf("Starting dot product calculation (Assembly)...\n");
-	clock_t start_time_asm = clock();
+	for (int t = 0; t < 20; t++) {
+		clock_t start = clock();
+		vec_product_asm(vec1, vec2, &result_asm, n);
+		clock_t end = clock();
+		total_time_asm += ((float)(end - start)) / CLOCKS_PER_SEC;
+	}
+	float avg_time_asm = total_time_asm / 20.0f;
+
+	// printf("Starting dot product calculation (Assembly)...\n");
+	// clock_t start_time_asm = clock();
 
 	// Perform single dot product calculation (Assembly)
-	vec_product_asm(vec1, vec2, &result_asm, n);
+	// vec_product_asm(vec1, vec2, &result_asm, n);
 
-	clock_t end_time_asm = clock();
-	float execution_time_asm = ((float)(end_time_asm - start_time_asm)) / CLOCKS_PER_SEC;
+	// clock_t end_time_asm = clock();
+	// float execution_time_asm = ((float)(end_time_asm - start_time_asm)) / CLOCKS_PER_SEC;
 
 	printf("\n=== RESULTS ===\n");
 	printf("C Result:        %f\n", result_c);
 	printf("Assembly Result: %f\n", result_asm);
 	
 	printf("\n=== PERFORMANCE ===\n");
-	printf("C Time:          %f seconds (%.3f microseconds)\n", execution_time_c, execution_time_c * 1000000);
-	printf("Assembly Time:   %f seconds (%.3f microseconds)\n", execution_time_asm, execution_time_asm * 1000000);
+	// printf("C Time:          %f seconds (%.3f microseconds)\n", execution_time_c, execution_time_c * 1000000);
+	// printf("Assembly Time:   %f seconds (%.3f microseconds)\n", execution_time_asm, execution_time_asm * 1000000);
 	
+	printf("C Time:          %f seconds\n", avg_time_c);
+	printf("Assembly Time:   %f seconds\n", avg_time_asm);
+
 	// Free allocated memory
 	free(vec1);
 	free(vec2);
